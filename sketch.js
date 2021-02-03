85590d7a6314100d0efbec185071985f3807cc7e
mathew <pogbuji@yahoo.com>
2021-02-03 18:05:43 +0000

    API

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg = "sprites/bg.png"
+var score = 0
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+getbackgroundimage()
 }
 
 function setup(){
@@ -45,18 +46,25 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+   if(backgroundImg){
+       background(backgroundImg)
+   }
+   textSize(35)
+   fill("black")
+   text("score:"+score,width-300,50)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +93,17 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getbackgroundimage(){
+ var response = await fetch("https://worldtimeapi.org/api/timezone/Europe/London")
+ var datetime = responseJSON.datetime
+ var hour = datetime.slice(11,13)
+ if(hour >= 0600 && hour <= 1800){
+ bg = "sprites/bg.png"
+ }
+ else{
+     bg = "sprites/bg2.png"
+ }
+ backgroundImg = loadImage(bg)
 }
